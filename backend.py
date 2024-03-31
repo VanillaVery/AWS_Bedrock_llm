4959c2ccc58dd3b669b8d255a6c74d0ac4c06ced
yoojinyoon <dbsdbwls0602@naver.com>
2024-03-31 15:49:06 +0900

    .

--- backend.py
+++ backend.py
@@ -1,8 +1,13 @@
 import os
 from langchain.llms import bedrock
 from langchain_community.chat_models import BedrockChat
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import ConversationalRetrievalChain
+import pandas as pd
+from langchain.llms import BedrockEmbeddings
+from langchain.indexes import VectorstoreIndexCreator, FAISS
+from langchain_community.document_loaders import DataFrameLoader
+
 
 # 다른 모듈과 같이 맞물려 동작할 수 있게 해주는 모듈
 
@@ -19,16 +24,48 @@ def bedrock_chatbot():
     return llm
 
 
-def buff_memory():
+def get_memory():
     # 입력과 출력을 저장
-    buff_memory = bedrock_chatbot()
-    memory = ConversationBufferMemory(llm=buff_memory)
+    # buff_memory = bedrock_chatbot()
+    memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True)
     return memory
 
 
-def cnvs_chain(input_text, memory):
-    chain_data = bedrock_chatbot()
-    cnvs_chain = ConversationChain(llm=chain_data, memory=memory, verbose=True)
-    # 모델 모듈과 메모리 모듈을 엮어서 동작시킴
-    chat_reply = cnvs_chain.predict(input=input_text)
-    return chat_reply
+
+def get_rag_chat_response(input_text, memory, index): #chat client 함수
+    
+    llm = bedrock_chatbot()
+    
+    conversation_with_retrieval = ConversationalRetrievalChain.from_llm(llm, index.vectorstore.as_retriever(), memory=memory)
+    
+    chat_response = conversation_with_retrieval({"question": input_text}) #사용자 메시지, 기록 및 지식을 모델에 전달합니다.
+    
+    return chat_response['answer']
+
+
+def get_index():
+    # 예제 데이터 (책 제목과 소개 포함)
+    book_data = pd.DataFrame({
+        'title': ['Book Title 1', 'Book Title 2', ...],  # 책 제목
+        'description': ['Description 1', 'Description 2', ...]  # 책 소개
+    })
+
+    embeddings = BedrockEmbeddings(
+        credentials_profile_name=os.environ.get("BWB_PROFILE_NAME"),
+        region_name=os.environ.get("BWB_REGION_NAME"),
+        endpoint_url=os.environ.get("BWB_ENDPOINT_URL"),
+    )
+
+    # 각 책의 내용을 로드하기 위한 커스텀 로더 생성
+    loaders = [DataFrameLoader(text=row['title'] + " " + row['description']) for _, row in book_data.iterrows()]
+
+    index_creator = VectorstoreIndexCreator(
+        vectorstore_cls=FAISS,
+        embedding=embeddings,
+    )
+
+    index_from_loader = index_creator.from_loaders(loaders)
+
+    return index_from_loader
+
+index = get_index()
